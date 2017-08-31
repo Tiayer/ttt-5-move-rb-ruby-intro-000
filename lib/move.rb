9d5fc07568dd7e0c8ecea2f5eaae78679fdb222b
Leonard Chew <leochew@gmail.com>
2017-08-31 13:34:45 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(user_input)
   "#{user_input}".to_i - 1
   end
 
-  def move(array, input, value = "X")
-    array[input] = value
+  def move(array, user_input, value = "X")
+    array[user_input] = value
   end
